a76d23b91a3cbbdb2cf0e73a9c5c897ab0adab34
MannyD16 <139398471+MannyD16@users.noreply.github.com>
2025-03-30 18:42:32 -0400

    Initial project setup for Doze

---
+++ Doze/DozeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DozeApp.swift
+//  Doze
+//
+//  Created by Manuel Diaz on 3/30/25.
+//
+
+import SwiftUI
+
+@main
+struct DozeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DozeTests/DozeTests.swift
@@ -0,0 +1,17 @@
+//
+//  DozeTests.swift
+//  DozeTests
+//
+//  Created by Manuel Diaz on 3/30/25.
+//
+
+import Testing
+@testable import Doze
+
+struct DozeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
